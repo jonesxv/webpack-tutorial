ce9000280fce62a0703884cd25693e3e4d5712b5
Ryan Jones <ryanjones33@gmail.com>
2019-09-24 10:59:34 -0700

    File Loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Cat from './cat.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const cat = new Image();
+  cat.src = Cat;
+  
+  element.appendChild(cat);
 
   return element;
 }
