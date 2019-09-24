f181f9eaa19da142eed3c29506c95018796878e2
Ryan Jones <ryanjones33@gmail.com>
2019-09-24 11:05:17 -0700

    File Loader - XML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Cat from './cat.jpg';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
@@ -13,6 +14,8 @@ function component() {
   
   element.appendChild(cat);
 
+  console.log(Data);
+  
   return element;
 }
 
