020f793f03ed2414a85f8a2fdaffd7c10b4cf437
Ryan Jones <ryanjones33@gmail.com>
2019-09-24 13:42:25 -0700

    webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
-  devtools: 'inline-source-map',
+  devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
