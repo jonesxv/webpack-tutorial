3db91fa3ea2528e1f4a10544a2cd11aa6de9f122
Ryan Jones <ryanjones33@gmail.com>
2019-09-24 13:26:58 -0700

    Output Management Init

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Cat from './cat.jpg';
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  const cat = new Image();
-  cat.src = Cat;
-  
-  element.appendChild(cat);
-
-  console.log(Data);
-  
   return element;
 }
 
